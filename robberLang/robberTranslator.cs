7366452690fcf113af7a1969084aeb0069dcde5a
Martin Eriksson <martineriksson8@gmail.com>
2018-09-26 14:12:42 +0200

    updated UI, started coding translating method

---
+++ robberLang/robberTranslator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System;
+namespace Core
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToRobber(string raw)
+        {
+            //Checking if the string is empty. Otherwise we proceed.
+            if (string.IsNullOrWhiteSpace(raw))
+            {
+                return "";
+            }
+            else
+            {
+                raw = raw.ToUpperInvariant();
+            }
+            var 
+
+            }
+        }
+    }
+}
